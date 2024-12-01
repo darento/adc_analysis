c5c511ecb5f5b1d778b2e1f35eb7fcccd435ad63
darento <dsartiom@gmail.com>
2024-12-01 22:16:01 +0100

    Added libs to env.

--- main.py
+++ main.py
@@ -4,30 +4,7 @@ from src.adc_reader import read_raw_file
 
 
 def main():
-    # Example usage
-    # Read the file path from command-line arguments
-    file_path = sys.argv[1]
-    # order = [1, 2, 3, 4, 5, 6, 7, 8, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
-    # order = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
-    order = range(33)
-    for event in read_raw_file(file_path):
-        ctrl = event["CTRL"]
-        digital_signals = event["D3-D0"]
-        timestamp = event["Timestamp"]
-        s = event["S"]
-        module_id = event["ModuleID"]
-        adc_channels = event["ADC Channels"]
-        print(f"CTRL: {ctrl}")
-        print(f"D3-D0: {digital_signals}")
-        print(f"Timestamp: {timestamp}")
-        print(f"S: {s}")
-        print(f"ModuleID: {module_id}")
-        print("ADC Channels: ", end="")
-        for i in order:
-            print(f"{adc_channels[i]} ", end="")
-        print()
-        print(f"Length of ADC Channels: {len(adc_channels)}")
-        input("Press Enter to continue...")
+    pass
 
 
 if __name__ == "__main__":
