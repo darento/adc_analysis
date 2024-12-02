59863af22e46552160d2340353a18b1894841fdd
darento <dsartiom@gmail.com>
2024-12-02 15:09:39 +0100

    Refactorized so one can yield bunch by bunch or make use of the worker/writer process.

--- src/adc_reader.py
+++ src/adc_reader.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 import os
 import matplotlib.pyplot as plt
 from multiprocessing import Process, Queue
-from typing import Iterator, Tuple, List
+from typing import Generator, Iterator, Tuple, List
 
 
 def _parse_header(chunk: bytes) -> dict:
@@ -40,6 +40,7 @@ def _parse_header(chunk: bytes) -> dict:
         "ModuleID": module_id,
     }
 
+
 def _parse_adc_channels(adc_data: bytes, order: list) -> List[int]:
     """
     Parses the ADC channel values from a data chunk.
@@ -53,7 +54,7 @@ def _parse_adc_channels(adc_data: bytes, order: list) -> List[int]:
     adc_channels = [0] * 33
     bytes_data = struct.unpack(f"{len(adc_data)}B", adc_data)
     for i in range(0, len(adc_data) - 2, 3):
-        byte1, byte2, byte3 = bytes_data[i:i + 3]
+        byte1, byte2, byte3 = bytes_data[i : i + 3]
 
         adc1 = ((byte1 << 4) | (byte2 >> 4)) & 0xFFF
         adc2 = ((byte2 & 0x0F) << 8) | byte3
@@ -67,55 +68,14 @@ def _parse_adc_channels(adc_data: bytes, order: list) -> List[int]:
     adc_channels[-1] = adc33
 
     # Send only the channels in the order specified
-    adc_channels = [adc_channels[i - 1] for i in order]
+    adc_channels = [adc_channels[i] for i in order]
 
     return adc_channels
 
 
-def writer_process(output_file: str, queue: Queue):
-    """
-    Writes coincidences (timestamps and ADC values) to a file.
-
-    Args:
-        output_file (str): Path to the output file.
-        queue (Queue): Queue to receive coincidences from worker processes.
-    """
-    with open(output_file, "w") as file:
-        while True:
-            data = queue.get()
-            if data == "DONE":
-                break
-            groups, adc_values = data
-            # write per columns in the file, where each line is a coincidence
-            for group, adc_value in zip(groups, adc_values):
-                file.write("\t".join(map(str, group)) + "\t" + "\t".join(map(str, adc_value)) + "\n")
-
-
-            
-
-
-def _read_buffer(file, start: int, end: int, buffer_size: int) -> bytes:
-    """
-    Reads a buffer from the file.
-
-    Args:
-        file: File object.
-        start (int): Start byte position.
-        end (int): End byte position.
-        buffer_size (int): Size of the buffer to read.
-
-    Returns:
-        bytes: The read buffer.
-    """
-    file.seek(start)
-    buffer = b""
-    while start < end:
-        read_size = min(buffer_size, end - start)
-        buffer += file.read(read_size)
-        start += read_size
-    return buffer
-
-def _extract_data(buffer: bytes, event_size: int, order: list) -> Tuple[List[Tuple[int, int]], List[List[int]]]:
+def _extract_data(
+    buffer: bytes, event_size: int, order: list
+) -> Tuple[List[Tuple[int, int]], List[List[int]]]:
     """
     Extracts timestamps and ADC data from the buffer.
 
@@ -135,7 +95,7 @@ def _extract_data(buffer: bytes, event_size: int, order: list) -> Tuple[List[Tup
     for i in range(0, len(buffer), event_size):
         if i + event_size > len(buffer):
             break
-        chunk = buffer[i:i + event_size]
+        chunk = buffer[i : i + event_size]
         header = _parse_header(chunk)
         if header["S"] == 1:
             continue
@@ -143,10 +103,13 @@ def _extract_data(buffer: bytes, event_size: int, order: list) -> Tuple[List[Tup
         timestamps.append((valid_event_index, header["Timestamp"]))
         adc_data_list.append(adc_data)
         valid_event_index += 1
-        
+
     return timestamps, adc_data_list
 
-def _detect_coincidences(timestamps: List[Tuple[int, int]], adc_data_list: List[List[int]], threshold: int) -> List[Tuple[List[Tuple[int, int]], List[List[int]]]]:
+
+def _detect_coincidences(
+    timestamps: List[Tuple[int, int]], adc_data_list: List[List[int]], threshold: int
+) -> List[Tuple[List[Tuple[int, int]], List[List[int]]]]:
     """
     Detects coincidences in the data.
 
@@ -164,27 +127,29 @@ def _detect_coincidences(timestamps: List[Tuple[int, int]], adc_data_list: List[
     for i in range(1, len(timestamps)):
         delta = timestamps[i][1] - tstp_group
         if delta <= threshold and delta >= 0:
-            current_group.append((timestamps[i][0], timestamps[i][1]))   
+            current_group.append((timestamps[i][0], timestamps[i][1]))
         elif delta < 0:
             if len(current_group) > 1:
-                coincident_adc_values = [adc_data_list[j[0]] for j in current_group[0:2]]
-                coincidences.append((current_group[0:2], coincident_adc_values))
-            current_group = [(timestamps[i][0], timestamps[i][1])]    
-            tstp_group = timestamps[i][1]                  
-        else:
-            if len(current_group) > 1:          
-                coincident_adc_values = [adc_data_list[j[0]] for j in current_group[0:2]]
+                coincident_adc_values = [
+                    adc_data_list[j[0]] for j in current_group[0:2]
+                ]
                 coincidences.append((current_group[0:2], coincident_adc_values))
             current_group = [(timestamps[i][0], timestamps[i][1])]
             tstp_group = timestamps[i][1]
-
-    if len(current_group) > 1:
-        coincident_adc_values = [adc_data_list[j[0]] for j in current_group]
-        coincidences.append((current_group, coincident_adc_values))
-
+        else:
+            if len(current_group) > 1:
+                coincident_adc_values = [
+                    adc_data_list[j[0]] for j in current_group[0:2]
+                ]
+                coincidences.append((current_group[0:2], coincident_adc_values))
+            current_group = [(timestamps[i][0], timestamps[i][1])]
+            tstp_group = timestamps[i][1]
     return coincidences
 
-def _send_coincidences_to_queue(coincidences: List[Tuple[List[Tuple[int, int]], List[List[int]]]], queue: Queue):
+
+def _send_coincidences_to_queue(
+    coincidences: List[Tuple[List[Tuple[int, int]], List[List[int]]]], queue: Queue
+):
     """
     Sends coincidences to the writer process via the queue.
 
@@ -192,10 +157,76 @@ def _send_coincidences_to_queue(coincidences: List[Tuple[List[Tuple[int, int]], 
         coincidences (List[Tuple[List[Tuple[int, int]], List[List[int]]]]): List of coincidences.
         queue (Queue): Queue to send coincidences to the writer process.
     """
-    for group, adc_values in coincidences:                
-        queue.put((group, adc_values))        
+    for group, adc_values in coincidences:
+        queue.put((group, adc_values))
 
-def process_segment(file_path: str, order: list, start: int, end: int, threshold: int, queue: Queue, event_size: int = 56):
+
+def writer_process(output_file: str, queue: Queue):
+    """
+    Writes coincidences (timestamps and ADC values) to a file.
+
+    Args:
+        output_file (str): Path to the output file.
+        queue (Queue): Queue to receive coincidences from worker processes.
+    """
+    with open(output_file, "w") as file:
+        while True:
+            data = queue.get()
+            if data == "DONE":
+                break
+            groups, adc_values = data
+            # write per columns in the file, where each line is a coincidence
+            for group, adc_value in zip(groups, adc_values):
+                file.write(
+                    "\t".join(map(str, group))
+                    + "\t"
+                    + "\t".join(map(str, adc_value))
+                    + "\n"
+                )
+
+
+def worker_process(
+    file_path: str,
+    start: int,
+    end: int,
+    num_ev_buf: int,
+    event_size: int,
+    order: list,
+    threshold: int,
+    queue: Queue,
+):
+    """
+    Processes the file and sends coincidences to the queue.
+
+    Args:
+        file_path (str): Path to the file.
+        start (int): Start byte position.
+        end (int): End byte position.
+        num_ev_buf (int): Size of the buffer to read.
+        event_size (int): Size of each event in bytes.
+        order (list): The order of the ADC channels.
+        threshold (int): Threshold for coincidence detection.
+        queue (Queue): Queue to send coincidences to.
+    """
+    timestamp_list = []
+    cnt_chunks = 0
+
+    for timestamps, adc_data_list in read_file_chunks(
+        file_path, start, end, num_ev_buf, event_size, order
+    ):
+        # Detect coincidences
+        coincidences = _detect_coincidences(timestamps, adc_data_list, threshold)
+
+        # Send coincidences to the writer process
+        _send_coincidences_to_queue(coincidences, queue)
+
+        timestamp_list.extend(timestamps)
+        cnt_chunks += 1
+
+
+def read_file_chunks(
+    file_path: str, start: int, end: int, num_ev_buf: int, event_size: int, order: list
+) -> Generator[Tuple[List[Tuple[int, int]], List[List[int]]], None, None]:
     """
     Processes a segment of the file in smaller chunks, detects coincidences,
     and sends results to the writer process.
@@ -208,12 +239,17 @@ def process_segment(file_path: str, order: list, start: int, end: int, threshold
         queue (Queue): Queue to send coincidences to the writer process.
         event_size (int): Size of each event in bytes.
     """
-    buffer_size = 10000 * event_size  # Define a buffer size for smaller chunks
-    cnt_chunks = 1
-
+    buffer_size = num_ev_buf * event_size  # Define a buffer size for smaller chunks
     with open(file_path, "rb") as file:
+        file.seek(start)
         current_position = start
-        with tqdm(total=end - start, unit="B", unit_scale=True, unit_divisor=1024, desc=f"Process {start // (end - start)}") as pbar:
+        with tqdm(
+            total=end - start,
+            unit="B",
+            unit_scale=True,
+            unit_divisor=1024,
+            desc=f"Process {start // (end - start)}",
+        ) as pbar:
             while current_position < end:
                 # Read a smaller chunk from the file
                 read_size = min(buffer_size, end - current_position)
@@ -223,33 +259,36 @@ def process_segment(file_path: str, order: list, start: int, end: int, threshold
 
                 # Extract timestamps and ADC data
                 timestamps, adc_data_list = _extract_data(buffer, event_size, order)
-
-                # Detect coincidences
-                coincidences = _detect_coincidences(timestamps, adc_data_list, threshold)
-
-                # Send coincidences to the writer process
-                _send_coincidences_to_queue(coincidences, queue)
-                
-                # Break at certain number of events
-                # if cnt_chunks == 20:
-                #     break
-                cnt_chunks += 1
+                yield timestamps, adc_data_list
 
             pbar.close()
-
 
 
 if __name__ == "__main__":
     # Example usage
     # Read the file path from command-line arguments
     file_path = "P:/Valencia/I3M/Proyectos/DeepBrain/data/05022024_FOV0_0_600s.raw"
-    output_file = "P:/Valencia/I3M/Proyectos/DeepBrain/data/05022024_FOV0_0_600s_coincidences.txt"
+    output_file = (
+        "P:/Valencia/I3M/Proyectos/DeepBrain/data/05022024_FOV0_0_600s_coincidences.txt"
+    )
+    num_ev_buf = 1000
+    event_size = 56
     # order = [1, 2, 3, 4, 5, 6, 7, 8, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
-    order = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
-    num_processes = 6
+    # order = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+    order = range(33)
+    num_processes = 1
     threshold = 10000
     file_size = os.path.getsize(file_path)
     chunk_size = file_size // num_processes
+
+    for timestamps, adc_data_list in read_file_chunks(
+        file_path, 0, file_size, num_ev_buf, event_size, order
+    ):
+        for t, adc in zip(timestamps, adc_data_list):
+            print(t, adc)
+            input("Press Enter to continue...")
+
+    """
 
     # Create a Queue for communication
     queue = Queue()
@@ -263,7 +302,19 @@ if __name__ == "__main__":
     for i in range(num_processes):
         start = i * chunk_size
         end = (i + 1) * chunk_size if i < num_processes - 1 else file_size
-        process = Process(target=process_segment, args=(file_path, order, start, end, threshold, queue))
+        process = Process(
+            target=worker_process,
+            args=(
+                file_path,
+                start,
+                end,
+                num_ev_buf,
+                event_size,
+                order,
+                threshold,
+                queue,
+            ),
+        )
         processes.append(process)
         process.start()
 
@@ -274,3 +325,4 @@ if __name__ == "__main__":
     # Signal the writer to stop
     queue.put("DONE")
     writer.join()
+    """
